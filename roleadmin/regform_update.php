a39c62312c3a0706e765a14ee05699bd227a2151
agusbudionoprastyo <agna4605@gmail.com>
2024-06-12 12:02:15 +0700

    Update regform_update.php

--- roleadmin/regform_update.php
+++ roleadmin/regform_update.php
@@ -57,7 +57,7 @@ function updateRoom($connection, $pdfFilePath, $id, $room) {
             $pdf->Text(51, 40, $room);
         }
     }
-    $outputPdfPath = __DIR__ . $pdfFilePath;
+    $outputPdfPath = $pdfFilePath;
     $pdf->Output($outputPdfPath, 'F');
 
     // Update Database
