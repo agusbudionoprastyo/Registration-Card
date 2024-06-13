b5304764a77db6a0f2610992dc8ec40d7930b0cb
agusbudionoprastyo <agna4605@gmail.com>
2024-06-13 11:06:18 +0700

    Update getUnpairedDevice.php

--- guestfolio/getUnpairedDevice.php
+++ guestfolio/getUnpairedDevice.php
@@ -4,15 +4,14 @@ require_once 'helper/connection.php';
 
 // Query untuk mengambil token_id dari device yang statusnya 'unpaired'
 $query = mysqli_query($connection, "SELECT token_id FROM token_device WHERE status = '0' LIMIT 1");
-$row = mysqli_fetch_array($query);
 
-if ($row->num_rows > 0) {
+if (mysqli_num_rows($query) > 0) {
     // output data dari setiap baris
-    while($row = $result->fetch_assoc()) {
+    while($row = mysqli_fetch_assoc($query)) {
         echo $row["token_id"];
     }
 } else {
     echo "No unpaired devices found";
 }
-$conn->close();
+$connection->close();
 ?>
