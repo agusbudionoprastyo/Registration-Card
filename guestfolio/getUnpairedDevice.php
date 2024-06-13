8177af414033575368bce800713743370db3fbd3
agusbudionoprastyo <agna4605@gmail.com>
2024-06-13 11:26:06 +0700

    Update getUnpairedDevice.php

--- guestfolio/getUnpairedDevice.php
+++ guestfolio/getUnpairedDevice.php
@@ -1,17 +1,13 @@
 <?php
-// Koneksi ke database dan ambil data
 require_once 'helper/connection.php';
 
-// Query untuk mengambil token_id dari device yang statusnya 'unpaired'
 $query = mysqli_query($connection, "SELECT token_id FROM token_device WHERE status = '0' LIMIT 1");
 
 if (mysqli_num_rows($query) > 0) {
-    // output data dari setiap baris
-    while($row = mysqli_fetch_assoc($query)) {
-        echo $row["token_id"];
-    }
+    $row = mysqli_fetch_assoc($query);
+    echo json_encode(['token_id' => $row["token_id"]]); // Mengirim data sebagai JSON
 } else {
-    echo "No unpaired devices found";
+    echo json_encode(['error' => "No unpaired devices found"]);
 }
 $connection->close();
 ?>
